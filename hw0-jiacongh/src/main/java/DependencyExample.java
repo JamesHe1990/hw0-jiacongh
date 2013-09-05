3473bc2a97ab33d3b571052ec52338cbac2da3da
jiacongh <hejiacongtheone@gmail.com>
2013-09-04 20:59:21 -0400

    GHFGHF

--- hw0-jiacongh/src/main/java/DependencyExample.java
+++ hw0-jiacongh/src/main/java/DependencyExample.java
@@ -18,7 +18,7 @@ public class DependencyExample {
    */
   public static void main(String[] args) {
     TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
-    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader("I'm feeling good today"));
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
     System.out.println(tokenizer.tokenize());
   }
 }
