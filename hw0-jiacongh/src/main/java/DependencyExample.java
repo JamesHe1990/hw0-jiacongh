ccf522132b63690d2ea9d135c36eb88aa2aa70a2
jiacongh <hejiacongtheone@gmail.com>
2013-09-09 19:45:27 -0400

    any

--- hw0-jiacongh/src/main/java/DependencyExample.java
+++ hw0-jiacongh/src/main/java/DependencyExample.java
@@ -1,4 +1,5 @@
 import java.io.StringReader;
+import java.util.ArrayList;
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.objectbank.TokenizerFactory;
 import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
@@ -17,8 +18,14 @@ public class DependencyExample {
    *          Set the first argument as the sentence to be tokenized.
    */
   public static void main(String[] args) {
+    if(args.length==0){
+      args=new String[1];
+      args[0]=new String("I'm happy now!");
+    }
+    
     TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
-    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0])); 
+    ArrayList aa = new ArrayList();
     System.out.println(tokenizer.tokenize());
   }
 }
